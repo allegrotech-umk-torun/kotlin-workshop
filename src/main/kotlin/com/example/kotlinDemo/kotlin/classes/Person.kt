511e5b6473ba7a9081a949013f1eb9f50ef9e1b3
Mateusz Wasieczko <mateusz.wasieczko@allegro.pl>
2021-10-13 08:13:37 +0200

    zakomentowane

--- src/main/kotlin/com/example/kotlinDemo/kotlin/classes/Person.kt
+++ src/main/kotlin/com/example/kotlinDemo/kotlin/classes/Person.kt
@@ -16,26 +16,26 @@ class Person(
     }
 }
 
-open class PersonWithDefault(
-    val name: String,
-    var nickName: String? = null,
-    var isMarried: Boolean = false
-) {
+//open class PersonWithDefault(
+//    val name: String,
+//    var nickName: String? = null,
+//    var isMarried: Boolean = false
+//) {
+//
+//    val upperCaseName = name.uppercase()
+//
+//    fun getNicknameIfExists(): String {
+//        return nickName ?: "nick name is not set"
+//    }
+//
+//    override fun toString(): String {
+//        return "Person(name='$name', nickName=$nickName, isMarried=$isMarried, upperCaseName='$upperCaseName')"
+//    }
+//}
 
-    val upperCaseName = name.uppercase()
-
-    fun getNicknameIfExists(): String {
-        return nickName ?: "nick name is not set"
-    }
-
-    override fun toString(): String {
-        return "Person(name='$name', nickName=$nickName, isMarried=$isMarried, upperCaseName='$upperCaseName')"
-    }
-}
-
-class PersonWithDetails(name: String, nickName: String? = null, isMarried: Boolean = false, val placeOfBirth: String) : PersonWithDefault(name, nickName, isMarried) {
-
-    override fun toString(): String {
-        return "PersonWithDetails(name='$name', nickName=$nickName, isMarried=$isMarried, upperCaseName='$upperCaseName, placeOfBirth='$placeOfBirth')"
-    }
-}
+//class PersonWithDetails(name: String, nickName: String? = null, isMarried: Boolean = false, val placeOfBirth: String) : PersonWithDefault(name, nickName, isMarried) {
+//
+//    override fun toString(): String {
+//        return "PersonWithDetails(name='$name', nickName=$nickName, isMarried=$isMarried, upperCaseName='$upperCaseName, placeOfBirth='$placeOfBirth')"
+//    }
+//}
--- src/main/kotlin/com/example/kotlinDemo/kotlin/expresions/Expresions.kt
+++ src/main/kotlin/com/example/kotlinDemo/kotlin/expresions/Expresions.kt
@@ -4,26 +4,26 @@ fun maxKt(a: Int, b: Int): Int {
     return if (a > b) a else b
 }
 
-fun rangeKt(number: Int): String {
-    return when {
-        number < 0 -> "([)..., 0)"
-        number < 10 -> "<0, 10)"
-        else -> "<10, ...)"
-    }
-}
-
-fun numberName(number: Int): String {
-    return when(number) {
-        0 -> "zero"
-        1 -> "jeden"
-        2 -> "dwa"
-        else -> "jakaś liczba"
-    }
-}
-
-fun booleanName(boolean: Boolean): String {
-    return when(boolean) {
-        true -> "prawda"
-        false -> "fałsz"
-    }
-}
+//fun rangeKt(number: Int): String {
+//    return when {
+//        number < 0 -> "([)..., 0)"
+//        number < 10 -> "<0, 10)"
+//        else -> "<10, ...)"
+//    }
+//}
+//
+//fun numberName(number: Int): String {
+//    return when(number) {
+//        0 -> "zero"
+//        1 -> "jeden"
+//        2 -> "dwa"
+//        else -> "jakaś liczba"
+//    }
+//}
+//
+//fun booleanName(boolean: Boolean): String {
+//    return when(boolean) {
+//        true -> "prawda"
+//        false -> "fałsz"
+//    }
+//}
